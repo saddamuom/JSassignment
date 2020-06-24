5fa5c1fcb2dda96c6311749f2d90ff5509582bea
saddamuom <saddamuom74@gmail.com>
2020-06-24 21:44:40 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,564 @@
+//==================Chapter 21-25 Task 1 =====================
+
+
+//var first = "saddam";
+//var last  = "hussain";
+//var first = prompt("Enter your first name");
+//var last  = prompt("Enter your last name");
+//var fullName = "Sddam hussain"; 
+  //          alert("Greeting you by entering your full name saddam hussain");  
+
+
+         
+  //var firstName = "saddam";
+  //var lastName  = "hussain";
+  //var firstName = ["saddam"];
+  //var lastName  = ["hussain"];
+  //var fullName  = [];
+  //for ( i=0; i <firstName.length; i++){
+
+   // for ( j=0; j<lastName.length; j++){
+   //     fullName.push(firstName[i]+lastName[j]);
+  //  }
+  //}
+
+
+//==================Chapter 21-25 Task 2 =====================
+
+//var user = prompt("Enter your favourite mobile phone model");
+
+//document.write("My favourite mobile name is:" + user + "");
+//document.write("<br>");
+//document.write("Lengthof the string is:" + user.length+ "");
+
+
+//==================Chapter 21-25 Task 3 =====================
+
+//var letter = "pakistan";
+
+// document.write("string is:"+letter);
+// document.write("<br>");
+// document.write(" index of letter n is"+letter.indexOf("n"))
+
+//==================Chapter 21-25 Task 4 =====================
+
+// var name = "Hello world";
+// document.write("String is:" +name);
+// document.write("<br>");
+// document.write( "index of last 'l':"+name.lastIndexOf("l"));
+
+
+//==================Chapter 21-25 Task 5 =====================
+
+
+// var letter = "pakistani";
+
+// document.write("string is:"+letter);
+// document.write("<br>");
+// document.write("character at index 3 is:"+letter[3]);
+
+//==================Chapter 21-25 Task 6 =====================
+
+
+// var last="hussain";
+
+// document.write("saddam   "+last);
+
+
+//==================Chapter 21-25 Task 7 =====================
+
+
+// var city2="hyderabad";
+// var newcity=city2.replace("hyder","islam");
+// document.write(""+newcity);
+
+//==================Chapter 21-25 Task 8 =====================
+// var message = "Ali and Sami are best friends .They play cricket and football together";
+// var new1=message.replace("and","&");
+// document.write(""+new1);
+
+//==================Chapter 21-25 Task 9 =====================
+
+// var  number = "472";
+// document.write()
+
+
+//==================Chapter 21-25 Task 10 =====================
+
+// var name="peanuts";
+// var name1 = name.toUpperCase();
+// document.write(name1);
+
+//==================Chapter 21-25 Task 11 =====================
+
+// var name= prompt("Enter character");
+// var rep=name.toTitleCase();
+// document.write("title case:"+rep);
+
+//==================Chapter 21-25 Task 12 =====================
+
+// var numberAsNumber = 35.36;
+//  var numberAsString = numberAsNumber.toString();
+//  document.write("Result is:" + numberAsString);
+
+
+//==================Chapter 21-25 Task 13 =====================
+
+
+
+
+//==================Chapter 21-25 Task 14 =====================
+
+
+//var A = prompt("Enter the item you want");
+//  var A = ["cake", "apple pie", "cookie", "chips", "patties"]
+//  for( var a=0; a<A.length; a++)
+//  if (A[a]==="" +A)
+ 
+//  {
+//   document.write("item is found" + A[a]);
+//  }
+ 
+// else
+
+
+// document.write("Item is not found");
+
+
+//==================Chapter 21-25 Task 14 =====================
+
+
+// var pass = prompt("Enter your password");
+// if ((8-12 characters, at least 1 number, no spaces))
+
+
+//==================Chapter 21-25 Task 15 =====================
+
+//
+
+
+
+//==================Chapter 21-25 Task 16 =====================
+
+
+//var university = "University of Karachi";
+
+// let reversestr=function(str){
+//   return[...str]
+  
+// };
+// document.write("<br>");
+// document.write(reversestr(university));
+
+
+//==================Chapter 21-25 Task 17 =====================
+
+
+
+
+
+
+
+//==================Chapter 21-25 Task 18 =====================
+
+// count = 0;
+//  for(var i=0; i<text.length; i++)
+// {
+
+//   if(text.charAt(i)==='the'){
+
+//     count++;
+//   }
+// }
+
+// var text  = "The quick brown fax jump over the lazy dog";
+
+// document.write("" +count);
+
+
+
+
+
+
+//=========================END=======================///========
+//=========================END=======================///========
+
+
+
+
+
+
+
+
+//==================Chapter 26-30  Task 1  =====================
+
+
+// var a=54.55;
+//   document.write("Orignal number = "+a);
+//   document.write("<br>");
+//  document.write(" Floor Number = "+Math.floor(44.78));
+//   document.write("<br>");
+//  document.write("Ceil Number = "+Math.ceil(a));
+//   document.write("<br>");
+//   document.write("Round = "+Math.round(a));
+
+//==================Chapter 26-30  Task 2  =====================
+
+// var a=-8.55;
+//   document.write("Orignal number = "+a);
+//   document.write("<br>");
+//  document.write(" Floor Number = "+Math.floor(a));
+//   document.write("<br>");
+//  document.write("Ceil Number = "+Math.ceil(a));
+//   document.write("<br>");
+
+
+
+//==================Chapter 26-30  Task 3  =====================
+
+// var a=-4;
+// document.write(" Absalute value of "+a+" is "+Math.abs(a));
+
+
+//==================Chapter 26-30  Task 4 =====================
+
+
+// document.write("random Number "+Math.random());
+
+
+
+//==================Chapter 26-30  Task 5 =====================
+
+
+
+//==================Chapter 26-30  Task 6 =====================
+
+
+// var a=Math.floor(Math.random()*100)+1;
+
+// document.write(a);
+
+
+
+//==================Chapter 26-30  Task 7 =====================
+
+
+// var weight=prompt("Enter your Weight");
+// document.write(Math.floor(weight +" kgs"));
+// document.write("<br>");
+// document.write(Math.ceil(weight +" kgs"));
+// document.write("<br>");
+
+// document.write(weight);
+
+//======================END========================/////////=========.
+
+
+
+
+
+//==================Chapter 31-34  Task 1  =====================
+
+// var a = new Date();
+// document.write( a);
+
+
+//==================Chapter 31-34  Task 2  =====================
+
+
+  // var getmonths = ["January","February","March","April","May","June","July","August",
+  // "September","October","November","December"];
+  // var d = new Date();
+  // alert("current month is:" +getmonths[d.getMonth()]);
+
+
+//==================Chapter 31-34  Task 3  =====================
+
+
+
+
+//==================Chapter 31-34  Task 4  =====================
+
+//  var day1 = "satureday";
+//  var day2 = "sunday";
+
+//  var choice = prompt("Enter your day");
+//  if (choice===day1){
+//    document.write("its a Funday");
+//  }
+
+//  else if(choice===day2){
+//   document.write("its a Funday");
+//  }
+
+// else 
+
+// document.write("you type other day");
+
+
+//==================Chapter 31-34  Task 5  =====================
+
+
+// var days = prompt("enter any day from user");
+// if (days<=15){
+// document.write("First Fifteen days Of th Month");
+// }
+// else
+
+// document.write("Last days Of the Month");
+
+//==================Chapter 31-34  Task 6  =====================
+
+
+//  var a = new Date();
+//  document.write( a);
+
+
+//==================Chapter 31-34  Task 7  ====================
+
+// var time = prompt("Enter your required time ");
+
+// if( time>=12)
+// document.write("Its PM ");
+// else 
+
+// document.write("Its AM");
+
+
+//==================Chapter 31-34  Task 8  ====================
+
+
+
+
+
+//==================Chapter 31-34  Task 9  ====================
+
+
+//==================Chapter 31-34  Task 10  ====================
+
+
+//==================Chapter 31-34  Task 11 ====================
+
+
+
+//==================Chapter 31-34  Task 12  ====================
+
+// var currentdate=2020;
+// var hundred=100;
+// var result=currentdate-hundred;
+// document.write(" Hundred Year Age From Today The Date Was"+result);
+
+
+
+//==================Chapter 31-34  Task 13  ====================
+
+// var age=prompt("Enter Your Age");
+// var currentyear=2020;
+// var result=currentyear-age;
+// document.write(" Your date of birth Year is : "+result);
+
+
+//==================Chapter 31-34  Task 14  ====================
+
+// var user=prompt("Enter User Name");
+
+// var month=prompt("Enter Current Month");
+// var unit=prompt("Enter the Units");
+// var punit=prompt("Enter per unit charge");
+// var late=150;
+
+// var netamount=unit*punit;
+// var duedate=netamount+late;
+// document.write("Customer Name : " +user);
+// document.write("<br>");
+// document.write(" Month : " +month);
+// document.write("<br>");
+// document.write("Number of Units :" + unit);
+// document.write("<br>");
+// document.write("Amount before Due date :" +netamount);
+// document.write("<br>");
+// document.write("Amount after due date :" +duedate);
+
+// =======================END========================///=======.
+
+
+
+
+
+
+
+
+//==================Chapter 35-38  Task 1  ====================
+
+
+// var a = new Date();
+// document.write( a);
+
+
+
+//==================Chapter 35-38  Task 2  ====================
+
+
+// var first = prompt("Enter your First Name");
+// var last = prompt("Enter your last name");
+//  var fullName = first+last;
+//  document.write("Hi! your full name is:" + fullName );
+
+
+//==================Chapter 35-38  Task 3  ====================
+
+
+// var num1 = prompt("Enter your 1st number ");
+// var num2  = prompt("Enter your 2nd number");
+
+// var result = num1+num2;
+// document.write("Sum of given numbers is:" + result );
+
+
+
+//==================Chapter 35-38  Task 4  ====================
+
+//  var num1 = prompt("Enter your 1st number ");
+// var num2  = prompt("Enter your 2nd number");
+
+//  var result = num1*num2;
+//  document.write("Sum of given numbers is:" + result );
+//  document.write("<br>");
+
+ 
+//   var result = num1-num2;
+//   document.write("Difference of given numbers is:" + result );
+//   document.write("<br>");
+
+//   var result = num1%num2;
+//   document.write("Division of given numbers is:" + result );
+
+
+
+//==================Chapter 35-38  Task 5  ====================
+
+
+// var num  = prompt("Enter number from user");
+
+// var squar = num*num;
+// document.write("Squar:" +squar);
+
+
+//==================Chapter 35-38  Task 6  ====================
+
+
+// var num = prompt("Enter a number for finding factorial ");
+
+// var fact = 1;
+// for( a=1; a<=num; a++)
+// {
+// fact = fact*a;
+
+
+// }
+
+// document.write(fact);
+
+//==================Chapter 35-38  Task 7  ====================
+
+// // 7 count the number in your browser
+// var start=prompt("Enter start Number");
+// var end=prompt("Enter End Number");
+// for (var start;start<=end;start++)
+// {
+// document.write(" , "+start);
+// }
+
+
+//==================Chapter 35-38  Task 8  ====================
+
+// var base = prompt("Enter a base value");
+// var per  = prompt("Enter a perpendicular value");
+
+// function pythagorean(sideA, sideB){
+//   return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+// }
+
+// document.write("" +pythagorean(base, per));
+
+
+//==================Chapter 35-38  Task 9  ====================
+
+// var length = prompt("Enter a whole number for the length of  rectangle.");
+// var width = prompt("Enter a whole number for the width of  rectangle.");
+// function area(length, width) {
+//     return length * width;
+
+// }
+// document.write("The area of your rectangle is "  + area(length, width)); 
+
+
+//==================Chapter 35-38  Task 10  ====================   
+// var name = prompt("Enter  a word ");
+
+// function palindrome(str) {
+//   return true;
+// }
+// palindrome("eye");
+// document.write("your word is:" + name);
+
+
+//==================Chapter 35-38 Task 11  ====================   
+
+
+// find the longest string in an array
+
+// const str=" the quick brown fox jumped over the lazy dog";
+// const strarray=str.split(" ");
+// const storedarray=strarray.sort((stra,strb)=>{
+// return strb.length-stra.length;
+// }
+// )
+// document.write(storedarray[0]);
+//
+
+
+//==================Chapter 35-38 Task 12  ====================   
+
+// capatilize first letter of the string
+ 
+//  var string=prompt("*****Enter string *****");
+//  var string=string.charAt(0).toUpperCase()+ string.slice(1);
+//  alert (string);
+ 
+
+
+
+
+ //==================Chapter 35-38 Task 13  ====================
+
+// 13 occurence of specific letter in string
+// var str=" My Name is Saddam Hussain";
+// document.write(str);
+// document.write("<br>");
+// count=0;
+// for(var a=0;a<=str.length;a++)
+// {
+// if(str.charAt(a)==='a'){
+// count++
+// }
+// }
+// document.write(" letter 'a' is occur  "  + count + " times in above string");
+
+
+
+//==================Chapter 35-38 Task 14 ====================
+
+
+//  14 how to calculate radius and circumference of  circule 
+
+// var radius,aoc,coc;
+// radius=parseFloat(prompt("Enter radius of circle"));
+// aoc=Math.PI*radius*radius;
+// coc=2*Math.PI*radius;
+// document.write("Area of circle : =  "+aoc);
+// document.write("<br>");
+// document.write("circumference of circle : = "+coc);
+ 
+ 
+ 
